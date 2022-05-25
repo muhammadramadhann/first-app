d165369e4b8d646cb8c6baa1166d440e1e21ca6f
muhammadramadhann <ramadhannkurniawan@gmail.com>
2022-05-25 14:32:09 +0700

    clean up code format, etc.

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class App extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      user: [],
+        this.state = {
+            user: [],
+        };
+    }
+
+    getUser = async () => {
+        let response = await axios.get(
+            "https://jsonplaceholder.typicode.com/users/1"
+        );
+        this.setState({
+            user: response.data,
+        });
     };
-  }
 
-  getUser = async () => {
-    let response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users/1"
-    );
-    this.setState({
-      user: response.data,
-    });
-  };
+    componentDidMount() {
+        this.getUser();
+    }
 
-  componentDidMount() {
-    this.getUser();
-  }
-
-  render() {
-    const user = this.state.user;
-    return (
-      <div>
-        <p>My name is {user.name}</p>
-        <p>My username is {user.username}</p>
-      </div>
-    );
-  }
+    render() {
+        const user = this.state.user;
+        return (
+            <div className="container">
+                <p>My name is {user.name}</p>
+                <p>My username is {user.username}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
